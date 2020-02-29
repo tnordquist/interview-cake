192b14840f42204e245f37a196477400b6f9744c
Todd A Nordquist <toddnordquist144@gmail.com>
2020-02-29 08:47:57 -0700

    Basic solution for IsFirstcomeFirstServe

--- src/dev/tnordquist/arrayandstringmanipulation/MergeSortedArrays.java
+++ src/dev/tnordquist/arrayandstringmanipulation/MergeSortedArrays.java
@@ -4,40 +4,44 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class MergeSortedArrays {
 
   public static void main(String[] args) {
     MergeSortedArrays m = new MergeSortedArrays();
-    int[] mergeArr = m.mergeArray(new int[]{3, 4, 6, 10, 11, 15},new int[]{1, 5, 8, 12, 14, 19});
+    //    int[] mergeArr = m.mergeArray(new int[]{3, 4, 6, 10, 11, 15}, new int[]{1, 5, 8, 12, 14, 19});
+    //    int[] mergeArr = m.mergeArray(new int[]{-3, -2, -1, 0}, new int[]{1, 2, 3, 4});
+    //    int[] mergeArr = m.mergeArray(new int[]{3, 4, 6, 10, 11, 15}, new int[]{100,200,1001,100011});
+    //    int[] mergeArr = m.mergeArray(new int[]{}, new int[]{1});
+    int[] mergeArr = m
+        .mergeArray(new int[]{3, 4, 6, 10, 11, 15, 78, 112}, new int[]{13});
+    // More
     System.out.println(Arrays.toString(mergeArr));
+
   }
 
   private int[] mergeArray(int[] arr1, int[] arr2) {
 
-    int[] mergedArr;
+    int[] newArr = new int[arr1.length + arr2.length];
+    int ctA = 0;
+    int ctB = 0;
 
-    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i < newArr.length; ++i) {
 
-    for (Integer id : arr1
-    ) {
-      list.add(id);
+      if (ctA >= arr1.length && ctB < arr2.length) {
+        newArr[i] = arr2[ctB++];
+      } else if (ctB >= arr2.length && ctA < arr1.length) {
+        newArr[i] = arr1[ctA++];
+      } else {
+        if (arr1[ctA] <= arr2[ctB]) {
+          newArr[i] = arr1[ctA++];
+        } else {
+          newArr[i] = arr2[ctB++];
+        }
+      }
     }
-
-    for (Integer id : arr2
-    ) {
-      list.add(id);
-    }
-
-    Collections.sort(list);
-
-    Integer[] arr3 =  list.toArray(new Integer[list.size()]);
-
-    mergedArr = new int[arr3.length];
-    for (int i = 0; i < arr3.length; ++i) {
-      mergedArr[i] = arr3[i];
-    }
-    return mergedArr;
+    return newArr;
   }
 
 }
